1b41c162ba41f6b9216dae1032eb14cfafc078ca
Tijani Moro <tjhackx111@gmail.com>
2025-08-15 15:50:45 +0000

    initial commit

--- 172.06.84.0/signin.php
+++ 172.06.84.0/signin.php
@@ -57,79 +57,46 @@
 
  ?>
 
-    <!-- MAIN -->
-    <div class="row">
-        <div class="col-md-4 offset-md-4">
 
-            <div class="card">
-                <div class="card-body">
-                    <a href="../index" class="btn btn-sm btn-link float-right mb-3">
-                        visit site <span data-feather="cloud" class="ml-1"></span>
-                    </a>
-                    <h4 class="header-title mt-2" style="color: rgb(170, 184, 197);">Dashboard</h4>
-                    <form class="form-signin" method="POST">
+    <body class="d-flex align-items-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12" style="max-width: 25rem">
+                    <!-- Heading -->
+                    <h1 class="fs-1 text-center">Sign in</h1>
+
+                    <!-- Subheading -->
+                    <p class="lead text-center text-body-secondary">Access our dashboard and start tracking your tasks.</p>
+
+                    <!-- Form -->
+                    <form class="mb-5" method="POST">
                         <div class="text-center mb-4">
                             <code id="displayErrors"><?= $errorsMsg; ?></code>
                         </div>
-                        <div class="form-group">
-                            <input type="email" autocomplete="off" autofocus name="admin_email" id="admin_email" class="form-control form-control-sm form-control-dark" placeholder="Email" required value="<?= $email; ?>">
+                        <div class="mb-4">
+                            <label class="visually-hidden" for="email">Email Address</label>
+                            <input class="form-control" autocomplete="off" autofocus id="admin_email" name="admin_email" type="email" placeholder="Enter your email address..." />
                         </div>
-                        <div class="form-group">
-                            <input type="password" name="admin_pass" id="admin_pass" class="form-control form-control-sm form-control-dark" placeholder="Password" required >
+                        <div class="mb-4">
+                            <label class="visually-hidden" for="email">Email Address</label>
+                            <input class="form-control" id="admin_pass" name="admin_pass" type="email" placeholder="Enter your password..." value="<?= $pswd; ?>" />
                         </div>
-                        <div class="form-group">
-                            <input type="submit"  value="Crush it!" class="btn btn-sm btn-outline-secondary">
-                        </div>
+                        <button class="btn btn-secondary w-100" name="submitAdmin" id="submitAdmin" type="submit">Sign in</button>
                     </form>
+
+                    <!-- Text -->
+                    <p class="text-center text-body-secondary mb-0">Want to visit site? <a href="../index"> visit site</a>.</p>
+                    </div>
                 </div>
-            </div>
         </div>
-    </div>
-
-    <body class="d-flex align-items-center">
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-12" style="max-width: 25rem">
-          <!-- Heading -->
-          <h1 class="fs-1 text-center">Sign in</h1>
-
-          <!-- Subheading -->
-          <p class="lead text-center text-body-secondary">Access our dashboard and start tracking your tasks.</p>
-
-          <!-- Form -->
-          <form class="mb-5">
-            <div class="text-center mb-4">
-                            <code id="displayErrors"><?= $errorsMsg; ?></code>
-                        </div>
-            <div class="mb-4">
-              <label class="visually-hidden" for="email">Email Address</label>
-              <input class="form-control" autocomplete="off" autofocus id="admin_email" name="admin_email" type="email" placeholder="Enter your email address..." />
-            </div>
-             <div class="mb-4">
-              <label class="visually-hidden" for="email">Email Address</label>
-              <input class="form-control" id="admin_pass" name="admin_pass" type="email" placeholder="Enter your password..." value="<?= $pswd; ?>" />
-            </div>
-            <button class="btn btn-secondary w-100" name="submitAdmin" id="submitAdmin" type="submit">Sign in</button>
-          </form>
-
-          <!-- Text -->
-          <p class="text-center text-body-secondary mb-0">Don't have an account yet? <a href="./sign-up.html">Sign up</a>.</p>
-        </div>
-      </div>
-    </div>
      
     
     
     <!-- FOOTER -->
     <script type="text/javascript" src="media/files/jquery-3.3.1.min.js"></script>
-    <script type="text/javascript" src="media/files/popper-1.14.6.min.js"></script>
-    <script type="text/javascript" src="media/files/bootstrap.min.js"></script>
     <script type="text/javascript" src="media/files/feather.min.js"></script>
-
     <script type="text/javascript">
         feather.replace();
         $("#temporary").fadeOut(5000);
     </script>
 
-</body>
-</html>

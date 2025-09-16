f861a65f8bff73d2a94c2e1cc75331f587ad4c4d
Tijani Moro <tjhackx111@gmail.com>
2025-09-16 15:06:38 +0000

    initial commit

--- 172.06.84.0/report/full_election_report.php
+++ 172.06.84.0/report/full_election_report.php
@@ -156,22 +156,22 @@
 
 						$pdf->Cell(64,0,$ContID.$ContName,0,0);
 						$profile_picture = '../../media/uploadedprofile/' . $counts['cont_profile'];
-						try {
-							$safeImage = convertAndOverwriteToJpeg($profile_picture); 
-							$newFileName = basename($safeImage);
-							$newQuery = "UPDATE cont_details SET cont_profile = ? WHERE contestant_id = ?";
-							$statement = $conn->prepare($newQuery);
-							$statement->execute([$newFileName, $counts['contestant_id']]);
-							// dnd($newFileName);
-						} catch (Exception $e) {
-							echo "Error: " . $e->getMessage();
-						}
-
-						// $safeImage = forceToJpeg($profile_picture);
-						// dnd($safeImage);
-
 						if (file_exists($profile_picture)) {
-							$pdf->Cell(63,0,$pdf->Image($safeImage,$pdf->GetX(),$pdf->GetY(),20,22.5),0,0,'R');
+							try {
+								$safeImage = convertAndOverwriteToJpeg($profile_picture); 
+								$newFileName = basename($safeImage);
+
+								$newQuery = "UPDATE cont_details SET cont_profile = ? WHERE contestant_id = ?";
+								$statement = $conn->prepare($newQuery);
+								$statement->execute([$newFileName, $counts['contestant_id']]);
+								
+								$pdf->Cell(63,0,$pdf->Image($safeImage,$pdf->GetX(),$pdf->GetY(),20,22.5),0,0,'R');
+
+							} catch (Exception $e) {
+								// echo "Error: " . $e->getMessage();
+								$pdf->Cell(63,0,'Image file not found!' . $countNumberVotes . ' (' . round(($ContResult/$countNumberVotes) * 100,0,PHP_ROUND_HALF_UP) .'%)',0,0,'R');
+							}
+
 						} else {
 							$pdf->Cell(63,0,'Image file not found!' . $countNumberVotes . ' (' . round(($ContResult/$countNumberVotes) * 100,0,PHP_ROUND_HALF_UP) .'%)',0,0,'R');
 						}
